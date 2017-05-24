2986ab63db8ea85b39bc1b7538abb432310f5598
樊少华 <shaohua.chd@gmail.com>
2017-05-24 11:01:06 +0800

    删除多余的文件

--- TagSelectorView/Pods/Target Support Files/Pods-TagSelectorViewTests/Pods-TagSelectorViewTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TagSelectorViewTests : NSObject
-@end
-@implementation PodsDummy_Pods_TagSelectorViewTests
-@end
--- TagSelectorView/Pods/Target Support Files/Pods-TagSelectorViewUITests/Pods-TagSelectorViewUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TagSelectorViewUITests : NSObject
-@end
-@implementation PodsDummy_Pods_TagSelectorViewUITests
-@end
